47f01012eee8cbb7d863b8879b2c23f1628254a5
silvermk1 <perezvelasco.miguel@gmail.com>
2024-10-24 17:41:37 +0200

    Añadida Actity achivment list

---
+++ app/src/main/java/com/example/dam_g1_aplication/Mainclasses/AchivListActivity.kt
@@ -0,0 +1,21 @@
+package com.example.dam_g1_aplication.Mainclasses
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dam_g1_aplication.R
+
+class AchivListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_achiv_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
