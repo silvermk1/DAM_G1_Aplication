e73243aaa4eb73f41ad121303ff90398facc879a
marcramirez16 <marcramirezmoya@gmail.com>
2024-10-25 23:32:51 +0200

    interfaz grafica del perfil, aqui se muestran otros perfiles la categoria destacada y el boton de ajustes para editar la informacion y la configuracion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    //material components para estilos
+    implementation ("com.google.android.material:material:1.6.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
